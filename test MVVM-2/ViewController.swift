05ea9b2f30171e3c64b043db92af24250c138f53
EvgenLK <120525967+EvgenLK@users.noreply.github.com>
2023-07-05 08:00:26 +1000

    Initial Commit

---
+++ test MVVM-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test MVVM-2
+//
+//  Created by Evgenii Kutasov on 05.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
